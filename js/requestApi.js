7d35df6639ed4f8c4ead58cbdc449a2286c7e14b
Côme <94992419+Kiwozz@users.noreply.github.com>
2025-10-01 09:51:36 +0200

    ajout des filtres avancés, de la rechercher par ID, de la recherche par genre (seulement UI) et correction des bugs mineurs

--- js/requestApi.js
+++ js/requestApi.js
@@ -2,6 +2,7 @@ let API_KEY = prompt('Entre ta cle de API');
 
 
 
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -23,19 +24,21 @@ async function fetchData(url, options) {
 }
 
 async function rechercheParNom(name) { 
-    const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=' + name + '&sortBy=ranking&sortOrder=asc';
+    var sort = document.querySelector('input[name="sort"]:checked').value;
+    var sortBy = document.querySelector('input[name="sortBy"]:checked').value;
+    const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=' + name + '&sortBy=' +sort+'&sortOrder='+sortBy;
     // fetchData(url, options);
     return await fetchData(url, options);
 }
 
 
-function rechercheParID(id) {
-    const url = 'https://anime-db.p.rapidapi.com/anime/by-id/' + id;
-    return fetchData(url, options);
+async function rechercheParID(id) {
+    const url = 'https://anime-db.p.rapidapi.com/anime/by-id/' + id + '?page=1&size=1';
+    return await fetchData(url, options);
 }
 
 
-function rechercheParGenre(genre) {
+async function rechercheParGenre(genre) {
     const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&genres=' + genre + '&sortBy=ranking&sortOrder=asc';
-    return fetchData(url, options);
+    return await fetchData(url, options);
 }
--- js/search.js
+++ js/search.js
@@ -1,15 +1,76 @@
 var btnSearch = document.getElementById('searchBtn');
 var inputParametre = document.getElementById('parameter');
 
+var select = document.getElementById('filter');
+var paramField = document.getElementById('paramField');
+var checkboxGroup = document.getElementById('checkboxGroup');
+
 //detecter le btn de recherche
 
 btnSearch.addEventListener('click', async function() {
+
     param = inputParametre.value;
-    var reponse = await rechercheParNom(param);
+
+    var reponse;
+    clearCards();
+
+    if(select.value == 'byTitle'){
+        reponse = await rechercheParNom(param);
+    }else if(select.value == 'byGenre'){
+        reponse = await rechercheParGenre(param);
+    }else if(select.value == 'byId'){
+        reponse = await rechercheParID(param);
+        createCard(reponse );  
+    }
+
+    if (!reponse || !Array.isArray(reponse.data)) {
+        // console.error("La réponse ne contient pas de propriété 'data' valide :", reponse);
+        alert("Aucun résultat trouvé ou erreur lors de la recherche.");
+        return;
+    }
+
 
     let liste = [...reponse.data];
 
     liste.forEach(anime => {
         createCard(anime);
     });
+});
+
+
+select.addEventListener('change', function() {
+    var selectedOption = select.value;
+    switch (selectedOption) {
+        case 'byId':
+            inputParametre.placeholder = 'Ex: 21';
+            paramField.style.display = 'flex';
+            checkboxGroup.style.display = 'none';
+            break;
+        case 'byTitle':
+            inputParametre.placeholder = 'Ex: Naruto';
+            paramField.style.display = 'flex';
+            checkboxGroup.style.display = 'none';
+            break;
+        case 'byGenre':
+            console.log("genre");
+            paramField.style.display = 'none';
+            checkboxGroup.style.display = 'block';
+            break;
+        default:
+            paramField.style.display = 'flex';
+            checkboxGroup.style.display = 'none';
+    }
+});
+
+
+
+document.getElementById('toggleMoreFilter').addEventListener('click', function() {
+    const moreFilter = document.getElementById('moreFilter');
+    if (moreFilter.style.display === 'none' || moreFilter.style.display === '') {
+        moreFilter.style.display = 'flex';
+        this.textContent = 'Masquer les filtres avancés';
+    } else {
+        moreFilter.style.display = 'none';
+        this.textContent = 'Afficher les filtres avancés';
+    }
 });
